1023750cc526dc04422c1dc4616aedaa605be688
Keerti Srinagesh <skeerti@intuit.com>
2021-08-01 20:31:40 +0530

    removing works from nav bar

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
         <Header data={resumeData.main}/>
         <About data={resumeData.main}/>
         <Resume data={resumeData.resume}/>
-        <Portfolio data={resumeData.portfolio}/>
+        {/*<Portfolio data={resumeData.portfolio}/>*/}
         <Testimonials data={resumeData.testimonials}/>
         <Contact data={resumeData.main}/>
         <Footer data={resumeData.main}/>
